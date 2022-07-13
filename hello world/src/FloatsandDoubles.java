eeb7eec19e1ab3d394387db2be603b0448bedc05
maneeshreddy <87141940+Saimaneeshkumarreddy@users.noreply.github.com>
2022-07-13 12:15:33 +0530

    Add files via upload

---
+++ hello world/src/FloatsandDoubles.java
@@ -0,0 +1,28 @@
+
+public class FloatsandDoubles {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Min float value" + Float.MIN_VALUE);
+		System.out.println("Max float value" + Float.MAX_VALUE);
+		
+		System.out.println("Min Double value" + Double.MIN_VALUE);
+		System.out.println("Max Double value" + Double.MAX_VALUE);
+		
+		int val = 5;
+		
+		Float val2 = 5.2f;
+		
+		Double val3 = 5.2;
+		
+		val = 6;
+		val2 = 6f;
+		val3 = 6d;
+		System.out.println(val);
+		System.out.println(val2);
+		System.out.println(val3);
+		
+
+	}
+
+}
---
+++ hello world/src/s02variables.java
@@ -0,0 +1,10 @@
+
+public class s02variables {
+
+	public static void main(String[] args) {
+		int myint = 6;
+		System.out.println(myint);
+
+	}
+
+}
